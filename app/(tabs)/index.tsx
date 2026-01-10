c37b5ceba3c2457331744f0ae1ea8ebc7d4c876c
Andrei Lineishchikov <andrei.l@fastmail.com>
2026-01-11 00:21:22 +0100

    remove old

--- app/(tabs)/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from "expo-router";
-
-export default function HomeScreen() {
-  // It seems expo dev server always tries to load index route on bundle refresh
-  return <Redirect href="/(tabs)/artists" />;
-}
