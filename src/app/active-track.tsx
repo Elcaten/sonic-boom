b31ee9bcd13608c0f414794da1f1b073bd61fe3a
Andrei Lineishchikov <andrei.l@fastmail.com>
2026-05-26 21:34:01 +0200

    active track: work in progress

--- src/app/active-track.tsx
+++ src/app/active-track.tsx
@@ -1,11 +1,12 @@
 import { CoverArt } from "@/components/CoverArt";
 import { ProgressSlider } from "@/components/ProgressSlider";
-import { ThemedSafeAreaView } from "@/components/themed/themed-safe-area-view";
 import { useColors } from "@/context/app-context";
 import { usePlayerQueue } from "@/track-player/use-player-queue";
-import { Button, Host, HStack, Image, List, Text } from "@expo/ui/swift-ui";
-import { controlSize, font, listStyle, symbolEffect } from "@expo/ui/swift-ui/modifiers";
+import { Column, Row } from "@expo/ui";
+import { Button, Host, Image, RNHostView, Spacer, Text, VStack } from "@expo/ui/swift-ui";
+import { controlSize, font, foregroundStyle, padding } from "@expo/ui/swift-ui/modifiers";
 import TrackPlayer, { useActiveMediaItem, useIsPlaying } from "@rntp/player";
+import { useRouter } from "expo-router";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 function PreviousButton({ size }: { size: 32 | 48 }) {
@@ -18,17 +19,13 @@ function PreviousButton({ size }: { size: 32 | 48 }) {
 
 function PlayPauseButton({ size }: { size: 32 | 48 }) {
   const isPlaying = useIsPlaying();
+
   return (
     <Button
       modifiers={[controlSize("large")]}
       onPress={() => (isPlaying ? TrackPlayer.pause() : TrackPlayer.play())}
     >
-      <Image
-        systemName={isPlaying ? "pause.fill" : "play.fill"}
-        color="primary"
-        size={size}
-        modifiers={[symbolEffect({ effect: "scale" })]}
-      />
+      <Image systemName={isPlaying ? "pause.fill" : "play.fill"} color="primary" size={size} />
     </Button>
   );
 }
@@ -44,6 +41,7 @@ function NextButton({ size }: { size: 32 | 48 }) {
 const capturePan = Gesture.Pan().minDistance(1);
 
 export default function ActiveTrackModal() {
+  const router = useRouter();
   const colors = useColors();
   const activeTrack = useActiveMediaItem();
   const { queue } = usePlayerQueue();
@@ -57,89 +55,128 @@ export default function ActiveTrackModal() {
     }
   };
 
-  const handleMoveItem = async (fromIndex: number, toIndex: number) => {
-    const savedActiveTrack = TrackPlayer.getActiveMediaItem();
-    const savedProgress = TrackPlayer.getProgress();
+  // const handleMoveItem = async (fromIndex: number, toIndex: number) => {
+  //   const savedActiveTrack = TrackPlayer.getActiveMediaItem();
+  //   const savedProgress = TrackPlayer.getProgress();
 
-    const queue = TrackPlayer.getQueue();
-    const newQueue = [...queue];
-    const [movedItem] = newQueue.splice(fromIndex, 1);
-    newQueue.splice(toIndex, 0, movedItem);
-    TrackPlayer.setMediaItems(newQueue);
+  //   const queue = TrackPlayer.getQueue();
+  //   const newQueue = [...queue];
+  //   const [movedItem] = newQueue.splice(fromIndex, 1);
+  //   newQueue.splice(toIndex, 0, movedItem);
+  //   TrackPlayer.setMediaItems(newQueue);
 
-    const indexToSkipTo = newQueue.findIndex((item) => item.mediaId === savedActiveTrack?.mediaId);
-    if (indexToSkipTo !== -1) {
-      TrackPlayer.skipToIndex(indexToSkipTo);
-      TrackPlayer.seekTo(savedProgress.position);
+  //   const indexToSkipTo = newQueue.findIndex((item) => item.mediaId === savedActiveTrack?.mediaId);
+  //   if (indexToSkipTo !== -1) {
+  //     TrackPlayer.skipToIndex(indexToSkipTo);
+  //     TrackPlayer.seekTo(savedProgress.position);
+  //   }
+  //   TrackPlayer.play();
+  // };
+
+  // const handleDeleteItem = async (index: number) => {
+  //   const savedActiveTrack = TrackPlayer.getActiveMediaItem();
+  //   const savedProgress = TrackPlayer.getProgress();
+
+  //   const queue = TrackPlayer.getQueue();
+  //   const newQueue = [...queue];
+  //   newQueue.splice(index, 1);
+  //   TrackPlayer.setMediaItems(newQueue);
+
+  //   const indexToSkipTo = newQueue.findIndex((item) => item.mediaId === savedActiveTrack?.mediaId);
+  //   if (indexToSkipTo !== -1) {
+  //     TrackPlayer.skipToIndex(indexToSkipTo);
+  //     TrackPlayer.seekTo(savedProgress.position);
+  //   }
+  //   TrackPlayer.play();
+  // };
+
+  const handleAlbumPress = () => {
+    const albumId = activeTrack?.extras?.albumId;
+    const artistId = activeTrack?.extras?.artistId;
+
+    if (!albumId || typeof albumId !== "string" || !artistId || typeof artistId !== "string") {
+      return;
     }
-    TrackPlayer.play();
+
+    router.replace({
+      pathname: "/(tabs)/artists/[artistId]/albums/[albumId]/tracks",
+      params: { albumId: albumId, artistId: artistId },
+    });
   };
 
-  const handleDeleteItem = async (index: number) => {
-    const savedActiveTrack = TrackPlayer.getActiveMediaItem();
-    const savedProgress = TrackPlayer.getProgress();
-
-    const queue = TrackPlayer.getQueue();
-    const newQueue = [...queue];
-    newQueue.splice(index, 1);
-    TrackPlayer.setMediaItems(newQueue);
-
-    const indexToSkipTo = newQueue.findIndex((item) => item.mediaId === savedActiveTrack?.mediaId);
-    if (indexToSkipTo !== -1) {
-      TrackPlayer.skipToIndex(indexToSkipTo);
-      TrackPlayer.seekTo(savedProgress.position);
+  const handleArtistPress = () => {
+    const artistId = activeTrack?.extras?.artistId;
+    if (!artistId || typeof artistId !== "string") {
+      return;
     }
-    TrackPlayer.play();
-  };
 
-  return (
-    <ThemedSafeAreaView
-      style={{
-        height: 300,
-        paddingTop: 100,
-        paddingHorizontal: 48,
-        backgroundColor: colors.systemBackground,
-        gap: 32,
-        alignItems: "stretch",
-        flex: 1,
-      }}
-    >
-      <CoverArt id={activeTrack?.extras?.albumId} size={256} />
-      <GestureDetector gesture={capturePan}>
-        <ProgressSlider />
-      </GestureDetector>
-      <Host style={{ flex: 1 }}>
-        <HStack spacing={48}>
-          <PreviousButton size={32} />
-          <PlayPauseButton size={48} />
-          <NextButton size={32} />
-        </HStack>
-      </Host>
-    </ThemedSafeAreaView>
-  );
+    router.replace({
+      pathname: "/(tabs)/artists/[artistId]/albums",
+      params: { artistId: artistId },
+    });
+  };
 
   return (
     <Host style={{ flex: 1 }}>
-      <List
-        modifiers={[listStyle("inset")]}
-        // TODO: add move and delete functionality
-        // moveEnabled
-        // onMoveItem={handleMoveItem}
-        // deleteEnabled
-        // onDeleteItem={handleDeleteItem}
-      >
-        {queue.map((item, index) => (
-          <Button key={item.mediaId} onPress={() => handlePress(index)}>
+      <VStack modifiers={[padding({ vertical: 48, horizontal: 48 })]}>
+        <RNHostView matchContents>
+          <CoverArt id={activeTrack?.extras?.albumId} size={256} />
+        </RNHostView>
+
+        {/* <ScrollView>
+          {queue.map((item, index) => (
+            <Button key={item.mediaId} onPress={() => handlePress(index)}>
+              <Text
+                modifiers={[
+                  font({ weight: item.mediaId === activeTrack?.mediaId ? "semibold" : "regular" }),
+                ]}
+              >
+                {item.title!}
+              </Text>
+            </Button>
+          ))}
+        </ScrollView> */}
+
+        <Spacer modifiers={[padding({ bottom: 12 })]} />
+
+        <Column alignment="center" spacing={4}>
+          <Button onPress={handleAlbumPress}>
             <Text
               modifiers={[
-                font({ weight: item.mediaId === activeTrack?.mediaId ? "semibold" : "regular" }),
+                font({ weight: "semibold", size: 24 }),
+                foregroundStyle({ type: "color", color: "primary" }),
               ]}
             >
-              {item.title!}
+              {activeTrack?.title}
             </Text>
           </Button>
-        ))}
-      </List>
+          <Button onPress={handleArtistPress}>
+            <Text
+              modifiers={[font({ size: 18 }), foregroundStyle({ type: "color", color: "primary" })]}
+            >
+              {activeTrack?.artist}
+            </Text>
+          </Button>
+        </Column>
+
+        <Spacer modifiers={[padding({ bottom: 12 })]} />
+
+        <RNHostView>
+          <GestureDetector gesture={capturePan}>
+            <ProgressSlider />
+          </GestureDetector>
+        </RNHostView>
+
+        <Spacer modifiers={[padding({ bottom: 12 })]} />
+
+        <Row alignment="center">
+          <PreviousButton size={32} />
+          <Spacer />
+          <PlayPauseButton size={48} />
+          <Spacer />
+          <NextButton size={32} />
+        </Row>
+      </VStack>
     </Host>
   );
 }
